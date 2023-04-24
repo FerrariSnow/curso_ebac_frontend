8c30d0869795be5d18f77e41e504d4b14e6683c3
Ferrari <ferraridev18@gmail.com>
2023-04-24 17:00:49 -0300

    'enviando_atividade'

---
+++ exercicio_ts/src/exerc.ts
@@ -0,0 +1,9 @@
+let multiplica = (a: number, b: number): string =>
+    `A multiplicação de ${a} e ${b} é: ${a * b}.`;
+
+console.log(multiplica(53, 35));
+
+let cumprimenta = (nome: string): string =>
+    `Olá, ${nome}, espero que esteja bem.`;
+
+console.log(cumprimenta("Ferrari"));
